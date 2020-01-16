d7a359a81e35f226fa532d4f35b3fa6b25bbf221
Unknown <bert.zylstra65@gmail.com>
2020-01-15 19:12:14 -0700

    Add Activity Set Selector

--- ActivitySchedulerFrontEnd/ColumnCollections/ColumnCollections.cs
+++ ActivitySchedulerFrontEnd/ColumnCollections/ColumnCollections.cs
@@ -1,9 +1,6 @@
 ﻿using Camp;
 using GridShared;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ActivitySchedulerFrontEnd.ColumnCollections
 {
@@ -11,7 +8,13 @@ namespace ActivitySchedulerFrontEnd.ColumnCollections
 	{
         public static Action<IGridColumnCollection<ActivityDefinition>> ActivityDefinitionColumns = c =>
         {
-            c.Add(ad => ad.Name).Titled("Name").SetWidth(20);
+            c.Add(ad => ad.Name).Titled(nameof(ActivityDefinition.Name)).SetWidth(20);
+            c.Add(ad => ad.MinimumCapacity).Titled(nameof(ActivityDefinition.MinimumCapacity)).SetWidth(5)
+                .RenderValueAs(ad => ad.MinimumCapacity > 0 ? ad.MinimumCapacity.ToString() : "");
+            c.Add(ad => ad.MaximumCapacity).Titled(nameof(ActivityDefinition.MaximumCapacity)).SetWidth(5)
+                .RenderValueAs(ad => ad.MaximumCapacity < int.MaxValue ? ad.MaximumCapacity.ToString() : "No Limit");
+            c.Add(ad => ad.OptimalCapacity).Titled(nameof(ActivityDefinition.OptimalCapacity)).SetWidth(5)
+                .RenderValueAs(ad => ad.OptimalCapacity > 0 ? ad.OptimalCapacity.ToString() : "");
         };
     }
 }
--- ActivitySchedulerFrontEnd/Services/ActivityDefinitionService.cs
+++ ActivitySchedulerFrontEnd/Services/ActivityDefinitionService.cs
@@ -14,9 +14,26 @@ namespace ActivitySchedulerFrontEnd.Services
 	{
 		private List<ActivityDefinition> _activityDefinitions = new List<ActivityDefinition>
 		{
-			new ActivityDefinition{ Name = "Water fun", MinimumCapacity=10, MaximumCapacity=20, OptimalCapacity=15 },
+			new ActivityDefinition{ Name = "Water fun", MinimumCapacity=10, MaximumCapacity=20 },
 			new ActivityDefinition{ Name = "Field games", MinimumCapacity=10, MaximumCapacity=40, OptimalCapacity=16 }
 		};
+		private List<string> _activitySetNames = new List<string> { "preload", "default" };
+
+		public ActivityDefinitionService()
+		{
+			try
+			{
+				var activityDefinitions = ActivityDefinition.ReadActivityDefinitions("DefaultActivities.xml");
+				if (activityDefinitions != null)
+				{
+					_activityDefinitions = activityDefinitions;
+				}
+			}
+			catch (Exception)
+			{
+				// TODO: nice log message here
+			}
+		}
 
 		public Task Delete(params object[] keys)
 		{
@@ -31,7 +48,10 @@ namespace ActivitySchedulerFrontEnd.Services
 		public ItemsDTO<ActivityDefinition> GetActivityDefinitionsGridRows(Action<IGridColumnCollection<ActivityDefinition>> columns, QueryDictionary<StringValues> query)
 		{
 			var server = new GridServer<ActivityDefinition>(_activityDefinitions, new QueryCollection(query),
-				true, "activityDefinitionsGrid", columns);
+				true, "activityDefinitionsGrid", columns)
+				.Sortable()
+				.Filterable()
+				.WithMultipleFilters();
 
 			// return items to displays
 			return server.ItemsToDisplay;
@@ -40,7 +60,10 @@ namespace ActivitySchedulerFrontEnd.Services
 		public ItemsDTO<ActivityDefinition> GetActivityDefinitionsGridRows(QueryDictionary<StringValues> query)
 		{
 			var server = new GridServer<ActivityDefinition>(_activityDefinitions, new QueryCollection(query),
-				true, "activityDefinitionGrid", null).AutoGenerateColumns();
+				true, "activityDefinitionGrid", null).AutoGenerateColumns()
+				.Sortable()
+				.Filterable()
+				.WithMultipleFilters();
 
 			// return items to displays
 			return server.ItemsToDisplay;
@@ -55,12 +78,17 @@ namespace ActivitySchedulerFrontEnd.Services
 		{
 			throw new NotImplementedException();
 		}
+
+		public IEnumerable<string> GetActivitySetNames()
+		{
+			return _activitySetNames;
+		}
 	}
 
 	public interface IActivityDefinitionService : ICrudDataService<ActivityDefinition>
 	{
 		ItemsDTO<ActivityDefinition> GetActivityDefinitionsGridRows(Action<IGridColumnCollection<ActivityDefinition>> columns, QueryDictionary<StringValues> query);
 		ItemsDTO<ActivityDefinition> GetActivityDefinitionsGridRows(QueryDictionary<StringValues> query);
-
+		IEnumerable<string> GetActivitySetNames();
 	}
 }
