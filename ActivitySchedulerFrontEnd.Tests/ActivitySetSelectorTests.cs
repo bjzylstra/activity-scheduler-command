0d871b2f4e8fb53045bbe1c09f266a47525e4f7a
Unknown <bert.zylstra65@gmail.com>
2020-02-04 20:37:26 -0700

    Add FrontEnd test framework
    ActivitySetSelectorTests proves out the framework is functioning.

---
+++ ActivitySchedulerFrontEnd.Tests/ActivitySetSelectorTests.cs
@@ -0,0 +1,56 @@
+﻿using ActivitySchedulerFrontEnd.Pages;
+using ActivitySchedulerFrontEnd.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Testing;
+using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ActivitySchedulerFrontEnd.Tests
+{
+	[TestFixture]
+	public class ActivitySetSelectorTests
+	{
+		private TestHost _host = new TestHost();
+		private readonly List<string> _activitySetNames = new List<string> { "one", "two", "three" };
+		private object _callBackValue;
+		private RenderedComponent<ActivitySetSelector> _component;
+
+		[SetUp]
+		public void Setup()
+		{
+			IActivityDefinitionService service = Substitute.For<IActivityDefinitionService>();
+			service.GetActivitySetNames().Returns(_activitySetNames);
+			_host.AddService(service);
+
+			EventCallbackFactory eventCallbackFactory = new EventCallbackFactory();
+			EventCallback<string> eventCallback = eventCallbackFactory.Create<string>(this, x =>
+			{
+				_callBackValue = x;
+			});
+			IDictionary<string, object> parameters = new Dictionary<string, object>
+			{
+				{"CurrentActivitySetChanged", eventCallback }
+			};
+			_component = _host.AddComponent<ActivitySetSelector>(parameters);
+		}
+
+		[Test]
+		public void SetSelector_ChangeValue_FiresCallback()
+		{
+			// Arrange
+			_callBackValue = string.Empty;
+
+			// Act
+			var selector = _component.Find("select");
+			var selectorValue = _activitySetNames[1];
+			selector.Change(selectorValue);
+
+			// Assert
+			Assert.That(_callBackValue, Is.EqualTo(selectorValue), "Value on callback");
+			Assert.That(_component.Instance.CurrentActivitySet, Is.EqualTo(selectorValue),
+				"CurrentActivitySet");
+		}
+	}
+}
